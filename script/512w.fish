7956385702dc0df02fb8d0d460dd10411635afa0
Lucas Garron <code@garron.net>
2024-05-05 14:49:48 -0700

    Update `fish` shebang.

--- script/512w.fish
+++ script/512w.fish
@@ -1,4 +1,4 @@
-#!/usr/bin/env fish
+#!/usr/bin/env -S fish --no-config
 cd ./img
 for file in *
   convert $file -resize 512x\> ../img-512w/(path change-extension -- "" $file)-512w(path extension $file)
